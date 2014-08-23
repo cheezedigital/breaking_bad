f09ed0b3f5261e11afce01037bfdca8fb530168a
Justin Veirs <justinveirs@gmail.com>
2014-08-23 16:54:02 -0400

    spec'n. hopefully dis shit legit

--- breaking.rb
+++ breaking.rb
@@ -11,6 +11,20 @@ class Breaking
       return 'But'
     elsif count == 5
       return 'Pent'
+    elsif count == 6
+      return 'Hex'
+    elsif count == 7
+      return 'Hept'
+    elsif count == 8
+      return 'Oct'
+    elsif count == 9
+      return 'Non'
+    elsif count == 10
+      return 'Dec'
+    elsif count == 11
+      return 'Undec'
+    elsif count == 12
+      return 'Dodec'
     else
       count
     end
--- breaking_spec.rb
+++ breaking_spec.rb
@@ -22,13 +22,35 @@ describe Breaking, 'moving chemicals' do
     end
 
     it 'takes the number of carbons and converts to a straight chain' do
-      expect(Breaking.chemiclas(5)).to eq 'Pent'
+      expect(Breaking.chemicals(5)).to eq 'Pent'
     end
 
     it 'takes the number of carbons and converts to a straight chain' do
-      expect(Breaking.chemcials(6)).to eq 'Hex'
+      expect(Breaking.chemicals(6)).to eq 'Hex'
     end
 
+    it 'takes the number of carbons and converts to a straight chain' do
+      expect(Breaking.chemicals(7)).to eq 'Hept'
+    end
 
+    it 'takes the number of carbons and converts to a straight chain' do
+      expect(Breaking.chemicals(8)).to eq 'Oct'
+    end
+
+    it 'takes the number of carbons and converts to a straight chain' do
+      expect(Breaking.chemicals(9)).to eq 'Non'
+    end
+
+    it 'takes the number of carbons and converts to a straight chain' do
+      expect(Breaking.chemicals(10)).to eq 'Dec'
+    end
+
+    it 'takes the number of carbons and converts to a straight chain' do
+      expect(Breaking.chemicals(11)).to eq 'Undec'
+    end
+
+    it 'takes the number of carbons and converts to a straight chain' do
+      expect(Breaking.chemicals(12)).to eq 'Dodec'
+    end
   end
 end
