b01367f3e5cdf515522828a8e4bb9385d909546b
Justin Veirs <justinveirs@gmail.com>
2014-08-23 16:26:28 -0400

    more spec'n, we'll see.

--- breaking.rb
+++ breaking.rb
@@ -3,6 +3,14 @@ class Breaking
   def self.chemicals(count)
     if count == 1
       return 'Meth'
+    elsif count == 2
+      return 'Eth'
+    elsif count == 3
+      return 'Prop'
+    elsif count == 4
+      return 'But'
+    elsif count == 5
+      return 'Pent'
     else
       count
     end
--- breaking_spec.rb
+++ breaking_spec.rb
@@ -5,13 +5,24 @@ require_relative 'breaking'
 describe Breaking, 'moving chemicals' do
 
   describe '.chemicals', :focus do
-    it 'takes the number of carbons and converts to straight chain' do
-      expect(Breaking.chemicals(1)).to eq Meth
+    it 'takes the number of carbons and converts to a straight chain' do
+      expect(Breaking.chemicals(1)).to eq 'Meth'
     end
 
-    it 'takes the number of carbons and converts to straight chain' do
-      expect(Breaking.chemicals(2)).to eq Eth
+    it 'takes the number of carbons and converts to a straight chain' do
+      expect(Breaking.chemicals(2)).to eq 'Eth'
     end
-    
+
+    it 'takes the number of carbons and coverts to a straight chain' do
+      expect(Breaking.chemicals(3)).to eq 'Prop'
+    end
+
+    it 'takes the numnber of carbons and converts to a straight chain' do
+      expect(Breaking.chemicals(4)).to eq 'But'
+    end
+
+    it 'takes the number of carbons and converts to a straight chain' do
+      expect(Breaking.chemiclas(5)).to eq 'Pent'
+
   end
 end
